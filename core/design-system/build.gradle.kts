207d8c8a43e1ba8356aa437cc5fecc95c6cad24d
Giovanni <giovanni.guiselini@foton.la>
2026-08-22 21:45:26 -0300

    Criando yaml para publicar AAR

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.compiler)
+    id("maven-publish")
 }
 
 android {
@@ -25,6 +26,10 @@ android {
     buildFeatures {
         compose = true
     }
+
+    publishing {
+        singleVariant("release")
+    }
 }
 
 dependencies {
@@ -35,5 +40,29 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+}
 
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                // Define que o AAR da variante 'release' será publicado
+                from(components["release"])
+
+                groupId = "com.github.giovanning" // Geralmente o seu namespace ou com.github.seu-usuario
+                artifactId = "core-design-system"
+                version = "1.0.0"
+            }
+        }
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/giovanning/CoreShared")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
 }
